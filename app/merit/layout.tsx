82cebb945576a084a3c0b117b0a8afe5e311fa80
abdul kabugu <kabuguabdul2@gmail.com>
2023-02-24 13:19:01 +0300

    initial  commit

---
+++ app/merit/layout.tsx
@@ -0,0 +1,64 @@
+"use client"
+import './globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { Chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { publicProvider } from 'wagmi/providers/public'
+import {mainnet, polygon, bscTestnet} from 'wagmi/chains'
+export const metadata = {
+  title: 'Merit',
+  description: 'Decentralized badge platform',
+}
+
+const { chains, provider } = configureChains(
+  [bscTestnet], // you can add more chains here like chain.mainnet, chain.optimism etc.
+  [
+    jsonRpcProvider({
+      rpc: () => {
+        return {
+          http: 'https://rpc.ankr.com/bsc_testnet_chapel', // go to https://www.ankr.com/protocol/ to get a free RPC for your network if you're not using Polygon
+        };
+      },
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Cyber Tube',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  
+  
+  
+  
+
+  return (
+    <section>
+       <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} modalSize="compact">
+      {children}
+      </RainbowKitProvider>
+      </WagmiConfig>
+      </section>
+      
+      
+     
+     
+        
+    
+  )
+}
---
+++ app/merit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        I'm  merit  page  here 
+    </div>
+  )
+}
